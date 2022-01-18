67dfd6caac7ba6a059f664b77e8be91d6c6900ca
thePieApp <henrywfyeung@gmail.com>
2022-01-18 20:20:40 +0800

    http to https

--- src/commons/web3.js
+++ src/commons/web3.js
@@ -2,5 +2,5 @@
 // export const ipfsContentRoot = 'https://ipfs.io/ipfs/'
 // export const ipfsNodeUrl = 'https://ipfs.infura.io:5001/api/v0';
 
-export const ipfsContentRoot = 'http://ipfs.gateway.radian.community/ipfs/'
-export const ipfsNodeUrl = 'http://ipfs.gateway.radian.community:5050';
+export const ipfsContentRoot = 'https://ipfs.gateway.radian.community/ipfs/'
+export const ipfsNodeUrl = 'https://ipfs.gateway.radian.community:5050';
