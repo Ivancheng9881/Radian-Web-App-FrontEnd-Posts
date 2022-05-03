64431e38333d79008c8cd9cfedeb804277a6b0c3
Ivan Cheng <ivanchengzhongyan@gmail.com>
2022-05-03 16:36:08 +0800

    updated text content

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,49 +3,84 @@ import { FC } from "react";
 import config from "../../commons/config";
 
 interface DefaultFooterProps {
-    disableGutter?: boolean
+  disableGutter?: boolean;
 }
 
-const DefaultFooter : FC<DefaultFooterProps> = (props) => {
-    const { disableGutter = false } = props 
+const DefaultFooter: FC<DefaultFooterProps> = (props) => {
+  const { disableGutter = false } = props;
 
-    return (
-        <div className={`rd-footer-root ${disableGutter ? 'rd-footer-root-no-gutter' : ''}`}>
-            <div className="rd-footer" >
-                <div className="rd-footer-content">
-                <Row gutter={[0, {xs: 12, sm: 24, md: 24}]}>
-                    <Col lg={12} md={24}> 
-                        <Space direction='vertical' size='large' >
-                            <div>
-                                <img className="rd-footer-logo" src={`${config.assets.cdn}/logo/logo_black.png`} alt='RADIAN logo' />
-                            </div>
-                            <Space className="rd-footer-social" direction="horizontal" size='large'>
-                                <a href='https://www.instagram.com/radiancommunity/' target={'_blank'} >
-                                    <img src={`${config.assets.cdn}/icon/instagram_white.png`} alt='follow us on Instagram' />
-                                </a>
-                                <a href='https://radiancommunity.medium.com/' target={'_blank'} >
-                                    <img src={`${config.assets.cdn}/icon/medium_white.png`} alt='follow us on Medium' />
-                                </a>
-                                <a href='https://t.me/RADIANCommunity' target={'_blank'} >
-                                    <img src={`${config.assets.cdn}/icon/discord_white.png`} alt='follow us on Discord' />
-                                </a>
-                                <a href='https://twitter.com/RADIANcommunity' target={'_blank'} >
-                                    <img src={`${config.assets.cdn}/icon/twitter_white.png`} alt='follow us on Twitter' />
-                                </a>
-                            </Space>
-                        </Space>
-                    </Col>
-                    <Col lg={12} md={24}>
-                        <Typography.Text className="rd-footer-text">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Habitant nisi, gravida mauris lacus scelerisque sit nunc. Integer nibh nunc integer eu mi. Nunc nisl quam vitae turpis a non. At viverra morbi nunc quis vitae fames sed lacus. Nunc consectetur ultricies condimentum proin lectus velit mattis. Nec, id interdum commodo urna, suspendisse et ut. Turpis sapien nisl augue auctor pretium odio senectus ut. Mauris turpis semperquam vitae turpis a non. At viverra morbi nunc quis vitae fames sed lacus. Nunc consectetur ultricies condimentum proin lectus velit mattis. Nec, id interdum commodo urna, suspendisse et ut. Turpis sapien nisl augue auctor pretium odio senectus ut. Mauris turpis semper
-                        </Typography.Text>
-                    </Col>
-                </Row>
+  return (
+    <div
+      className={`rd-footer-root ${
+        disableGutter ? "rd-footer-root-no-gutter" : ""
+      }`}
+    >
+      <div className="rd-footer">
+        <div className="rd-footer-content">
+          <Row gutter={[0, { xs: 12, sm: 24, md: 24 }]}>
+            <Col lg={12} md={24}>
+              <Space direction="vertical" size="large">
+                <div>
+                  <img
+                    className="rd-footer-logo"
+                    src={`${config.assets.cdn}/logo/logo_black.png`}
+                    alt="RADIAN logo"
+                  />
                 </div>
-
-            </div>
+                <Space
+                  className="rd-footer-social"
+                  direction="horizontal"
+                  size="large"
+                >
+                  <a
+                    href="https://www.instagram.com/radiancommunity/"
+                    target={"_blank"}
+                  >
+                    <img
+                      src={`${config.assets.cdn}/icon/instagram_white.png`}
+                      alt="follow us on Instagram"
+                    />
+                  </a>
+                  <a
+                    href="https://radiancommunity.medium.com/"
+                    target={"_blank"}
+                  >
+                    <img
+                      src={`${config.assets.cdn}/icon/medium_white.png`}
+                      alt="follow us on Medium"
+                    />
+                  </a>
+                  <a href="https://t.me/RADIANCommunity" target={"_blank"}>
+                    <img
+                      src={`${config.assets.cdn}/icon/discord_white.png`}
+                      alt="follow us on Discord"
+                    />
+                  </a>
+                  <a
+                    href="https://twitter.com/RADIANcommunity"
+                    target={"_blank"}
+                  >
+                    <img
+                      src={`${config.assets.cdn}/icon/twitter_white.png`}
+                      alt="follow us on Twitter"
+                    />
+                  </a>
+                </Space>
+              </Space>
+            </Col>
+            <Col lg={12} md={24}>
+              <Typography.Text className="rd-footer-text">
+                Liberate The Social Ecosystem  
+                <br>
+                </br>
+                Redefine The Ownership of Data
+              </Typography.Text>
+            </Col>
+          </Row>
         </div>
-    )
+      </div>
+    </div>
+  );
 };
 
 export default DefaultFooter;
--- src/page/passport/main/index.tsx
+++ src/page/passport/main/index.tsx
@@ -4,7 +4,10 @@ import DefaultFooter from "../../../components/Footer";
 import FriendCardCarousel from "../../../components/FriendCard/Carousel";
 import RadianPassport from "../../../components/Passport";
 import UserContext from "../../../utils/user/context/user.context";
-import { getProfileListErc, getProfileListCountErc } from "../../../utils/web3/contract/profileContract/erc";
+import {
+  getProfileListErc,
+  getProfileListCountErc,
+} from "../../../utils/web3/contract/profileContract/erc";
 import { ProfileList } from "../../../utils/web3/contract/profileContract/index.interface";
 import LandingSection from "../../landing/components/Section.components";
 import { PaginationType } from "../../../schema/helper.interface";
@@ -12,127 +15,149 @@ import ipfsUtils from "../../../utils/web3/ipfs/ipfs.utils";
 import { FullProfile } from "../../../schema/profile/profile.interface";
 import ScrollIndicator from "../../../components/ScrollIndicator";
 
+const PassportOverviewPage: FC = () => {
+  const userContext = useContext(UserContext);
 
-const PassportOverviewPage : FC = () => {
+  const [pagination, setPagination] = useState<PaginationType>({
+    count: 0,
+    skip: 0,
+    limit: 10,
+  });
+  const [profileList, setProfileList] = useState<FullProfile[]>([]);
+  const [atTop, setAtTop] = useState<boolean>(true);
 
-    const userContext = useContext(UserContext);
+  const resolveProfileFromIpfs = async (profiles: ProfileList) => {
+    const data = await Promise.all(
+      profiles.map(async (p) => {
+        let fullProfile = await ipfsUtils.getContentJson(p.identityID);
+        return {
+          ...fullProfile,
+          identityID: p.identityID,
+          profileID: p.profileID,
+        };
+      })
+    );
 
-    const [ pagination, setPagination ] = useState<PaginationType>({
-        count: 0,
-        skip: 0,
-        limit: 10,
-    });
-    const [ profileList, setProfileList ] = useState<FullProfile[]>([]);
-    const [ atTop, setAtTop ] = useState<boolean>(true);
+    return data;
+  };
 
-    const resolveProfileFromIpfs = async (profiles: ProfileList) => {
-        const data = await Promise.all(
-            profiles.map(async (p) => {
-                let fullProfile = await ipfsUtils.getContentJson(p.identityID);
-                return {
-                    ...fullProfile,
-                    identityID: p.identityID,
-                    profileID: p.profileID,
-                }
-            })
-        );
-
-        return data;
-    };
-
-    const getProfilesERC = async () : Promise<ProfileList> => {
-        let { count, skip, limit } = pagination;
-        let pageSize = skip;
-        if (skip >= count) {
-            // do nothing
-            return
-        } else if (skip + limit > count) {
-            pageSize = count - skip;
-        } else {
-            pageSize = count;
-        }
-        const profiles = await getProfileListErc(skip, pageSize);
-        return profiles
-    };
-
-    const getProfile = async () => {
-        try {
-            const ercData = await getProfilesERC();
-            if (ercData.length > 0) {
-                const profileData = await resolveProfileFromIpfs(ercData);
-                setProfileList(profileData);
-            }
-        } catch (error) {
-            console.log(error)
-        }
+  const getProfilesERC = async (): Promise<ProfileList> => {
+    let { count, skip, limit } = pagination;
+    let pageSize = skip;
+    if (skip >= count) {
+      // do nothing
+      return;
+    } else if (skip + limit > count) {
+      pageSize = count - skip;
+    } else {
+      pageSize = count;
     }
+    const profiles = await getProfileListErc(skip, pageSize);
+    return profiles;
+  };
 
-    const getProfileListCount = async () : Promise<void> => {
-        let count = await getProfileListCountErc();
-        setPagination({...pagination, count: count})
-    };
-
-    const handleScroll = () => {
-        const offsetTop = document.body.scrollTop || document.documentElement.scrollTop;
-        setAtTop(offsetTop <= 50)
+  const getProfile = async () => {
+    try {
+      const ercData = await getProfilesERC();
+      if (ercData.length > 0) {
+        const profileData = await resolveProfileFromIpfs(ercData);
+        setProfileList(profileData);
+      }
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    useEffect(() => {
-        getProfileListCount();
-    }, []);
+  const getProfileListCount = async (): Promise<void> => {
+    let count = await getProfileListCountErc();
+    setPagination({ ...pagination, count: count });
+  };
 
-    useEffect(() => {
-        if (pagination.count > 0) {
-            getProfile()
-        }
-    }, [pagination]);
+  const handleScroll = () => {
+    const offsetTop =
+      document.body.scrollTop || document.documentElement.scrollTop;
+    setAtTop(offsetTop <= 50);
+  };
 
-    useEffect(() => {
-        window.addEventListener('scroll', handleScroll);
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        }
-    }, [])
+  useEffect(() => {
+    getProfileListCount();
+  }, []);
 
-    return (
-        <>
-            <Layout.Content>
-                <LandingSection />
-                <div style={{zIndex: 1001, position: 'relative'}}>
-                    <div className="rd-section" style={{paddingTop: 60}}>
-                        <RadianPassport profile={userContext.profile} clickable/>
-                    </div>
-                    <div className="rd-section" style={{textAlign: 'center'}}>
-                        <Typography.Title level={4} className='rd-typo-marginless' >Welcome</Typography.Title>
-                        <Typography.Title level={1} className='rd-typo-marginless' >RADIAN Identity</Typography.Title>
-                        <div className="rd-typo-bounding" style={{width: 800, margin: 'auto'}} >
-                            <Typography.Text className="rd-typo-desc" >
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc lacus, pharetra nunc aliquam sem ut sed tincidunt. Neque tristique risus egestas condimentum integer odio. Eget vitae congue amet duis sed eget. Viverra feugiat amet tortor, nunc id turpis .
-                            </Typography.Text>
-                        </div>
-                        <br/>
-                        <Button type="primary" size="large" shape="round" >Social Now</Button>
-                    </div>
-                    <div className="rd-section" style={{textAlign: 'center'}}>
-                        <Typography.Title level={4} className='rd-typo-marginless' >Meet our</Typography.Title>
-                        <Typography.Title level={1} className='rd-typo-marginless' >Social Community</Typography.Title>
-                        <div className="rd-typo-bounding" style={{width: 800, margin: 'auto'}} >
-                            <Typography.Text className="rd-typo-desc" >
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc lacus, pharetra nunc aliquam sem ut sed tincidunt. Neque tristique risus egestas condimentum integer odio. Eget vitae congue amet duis sed eget. Viverra feugiat amet tortor, nunc id turpis .
-                            </Typography.Text>
-                        </div>
-                        <br/>
-                        {/* <Button type="primary" size="large" shape="round" >Find friends</Button> */}
-                        <div style={{marginTop: 15}}>
-                            {profileList.length > 0 && <FriendCardCarousel profiles={profileList}  />}
-                        </div>
-                    </div>
-                    <DefaultFooter disableGutter={true} />
-                    <ScrollIndicator hidden={!atTop} />
-                </div>
-            </Layout.Content>
-        </>
-    )
+  useEffect(() => {
+    if (pagination.count > 0) {
+      getProfile();
+    }
+  }, [pagination]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <>
+      <Layout.Content>
+        <LandingSection />
+        <div style={{ zIndex: 1001, position: "relative" }}>
+          <div className="rd-section" style={{ paddingTop: 60 }}>
+            <RadianPassport profile={userContext.profile} clickable />
+          </div>
+          <div className="rd-section" style={{ textAlign: "center" }}>
+            <Typography.Title level={4} className="rd-typo-marginless">
+              Welcome
+            </Typography.Title>
+            <Typography.Title level={1} className="rd-typo-marginless">
+              RADIAN PASSPORT
+            </Typography.Title>
+            <div
+              className="rd-typo-bounding"
+              style={{ width: 800, margin: "auto" }}
+            >
+              <Typography.Text className="rd-typo-desc">
+                RADIAN Passport is a Decentralised Identity that allows users to
+                bind wallet address(es) from multiple blockchains to a single
+                identity so that your on-chain presence can be aggregated. The
+                passport allows others to know who they are interacting with in
+                dApps.
+              </Typography.Text>
+            </div>
+            <br />
+            <Button type="primary" size="large" shape="round">
+              Social Now
+            </Button>
+          </div>
+          <div className="rd-section" style={{ textAlign: "center" }}>
+            <Typography.Title level={4} className="rd-typo-marginless">
+              Meet our
+            </Typography.Title>
+            <Typography.Title level={1} className="rd-typo-marginless">
+              Social Community
+            </Typography.Title>
+            <div
+              className="rd-typo-bounding"
+              style={{ width: 800, margin: "auto" }}
+            >
+              <Typography.Text className="rd-typo-desc">
+                Social in the Web3 way. You now have the freedom of speech, the
+                ownership of your identity, relationships and content.
+              </Typography.Text>
+            </div>
+            <br />
+            {/* <Button type="primary" size="large" shape="round" >Find friends</Button> */}
+            <div style={{ marginTop: 15 }}>
+              {profileList.length > 0 && (
+                <FriendCardCarousel profiles={profileList} />
+              )}
+            </div>
+          </div>
+          <DefaultFooter disableGutter={true} />
+          <ScrollIndicator hidden={!atTop} />
+        </div>
+      </Layout.Content>
+    </>
+  );
 };
 
 export default PassportOverviewPage;
