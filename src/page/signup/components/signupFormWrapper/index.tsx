633bbc97aee19eb7e86fc9fe5cedb31da3cc8c46
Ivan Cheng <ivanchengzhongyan@gmail.com>
2022-05-03 16:25:15 +0800

    responsiveness to signup page

--- src/page/signup/components/signupFormWrapper/index.tsx
+++ src/page/signup/components/signupFormWrapper/index.tsx
@@ -6,8 +6,8 @@ const SignupFormWrapper : FC = ({children}) => {
 
     return (
         <Row>
-            <Col span={6}></Col>
-            <Col span={18}>
+            <Col lg={6} md={0}></Col>
+            <Col lg={18} md={24}>
                 <GlassCard>
                     {children}
                 </GlassCard>
